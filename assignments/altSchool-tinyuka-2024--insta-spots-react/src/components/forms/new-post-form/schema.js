17f35e4a1207e78353218db8c91a510b240dbcef
AJ1732 <ejemeniboi@gmail.com>
2025-06-07 15:17:14 +0100

    remove

--- assignments/altSchool-tinyuka-2024--insta-spots-react/src/components/forms/new-post-form/schema.js
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const newPostSchema = z.object({
-  title: z.string().min(2, "Title is required"),
-  src: z.string().url("Enter a valid image URL"),
-});
--- assignments/altSchool-tinyuka-2024--insta-spots-react/src/components/svgs/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FavouriteIcon } from "./favourite";
-export { default as PencilIcon } from "./pencil";
-export { default as PlusIcon } from "./plus";
-export { default as InstaSpotsLogo } from "./logo";
